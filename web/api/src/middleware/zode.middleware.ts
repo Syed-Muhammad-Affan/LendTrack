31b6d3e93c31be689cc7c9ea2b3bfa0235aba024
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-08-17 14:02:45 +0500

    Update zode.middleware.ts

--- web/api/src/middleware/zode.middleware.ts
+++ web/api/src/middleware/zode.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { BadRequest } from '../errors/bad-request.js';
 import z from 'zod';
 
-const validate = (schema: z.ZodType) => {
+export const validate = (schema: z.ZodType) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
 
