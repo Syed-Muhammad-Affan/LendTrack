dbd82121aa9d63b75c9ef969af9402831ea71c7c
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-08-17 14:13:36 +0500

    Auth Controller

---
+++ web/api/src/controllers/auth.controllers.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { IAuthService } from '../services/auth.service/interface/auth.service.interface.js';
+import { IAuthController } from './interface/auth.controller.interface.js';
+import { registerSchema } from '../validators/auth.validator.js';
+import { BadRequest } from '../errors/bad-request.js';
+import { StatusCodes } from 'http-status-codes';
+
+export class AuthController implements IAuthController {
+  constructor(private readonly AuthService: IAuthService) {}
+
+  async register(req: Request, res: Response): Promise<Response> {
+    const data = await this.AuthService.register(req.body);
+
+    return res.status(StatusCodes.CREATED).json(data);
+  }
+
+  async login(req: Request, res: Response): Promise<Response> {
+    const data = await this.AuthService.login(req.body);
+
+    return res.status(StatusCodes.OK).json(data);
+  }
+}
