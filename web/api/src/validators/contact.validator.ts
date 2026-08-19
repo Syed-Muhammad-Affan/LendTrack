f66f991c744d849d9438c5967536a0782c0b1d0c
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-08-19 17:36:02 +0500

    Create contact.validator.ts

---
+++ web/api/src/validators/contact.validator.ts
@@ -0,0 +1,41 @@
+import z from 'zod';
+
+export const createContactSchema = z.object({
+  name: z.string().max(50).trim(),
+  email: z.email('Invalid email').optional(),
+  phone: z.string().trim().optional(),
+  notes: z.string().trim(),
+});
+
+export const contactParamsSchema = z.object({
+  contactId: z.string().min(1),
+});
+
+export const updateContactSchema = z
+  .object({
+    name: z.string().trim().min(1, 'Name cannot be empty').optional(),
+
+    email: z.string().email('Invalid email').optional(),
+
+    phoneNumber: z
+      .string()
+      .trim()
+      .min(1, 'Phone number cannot be empty')
+      .optional(),
+
+    notes: z.string().trim().optional(),
+  })
+  .refine(
+    (data) =>
+      data.name !== undefined ||
+      data.email !== undefined ||
+      data.phoneNumber !== undefined ||
+      data.notes !== undefined,
+    {
+      message: 'At least one field is required to update',
+    },
+  );
+
+export type CreateContactInout = z.infer<typeof createContactSchema>;
+export type UpdateContactInput = z.infer<typeof updateContactSchema>;
+export type ContactParamsInput = z.infer<typeof contactParamsSchema>;
