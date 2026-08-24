2c19896797fa71c26bc5977f015b7c2f63e0c24f
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-08-24 16:41:30 +0500

    add forbidden error

--- web/api/src/errors/index.ts
+++ web/api/src/errors/index.ts
@@ -1,6 +1,13 @@
 import { BadRequest } from './bad-request.js';
 import { CustomApiError } from './custom-error.js';
+import { Forbidden } from './forbidden.error.js';
 import { NotFound } from './not-found.js';
 import { Unauthenticated } from './unauthenticated.js';
 
-export default { CustomApiError, NotFound, BadRequest, Unauthenticated };
+export default {
+  CustomApiError,
+  NotFound,
+  BadRequest,
+  Unauthenticated,
+  Forbidden,
+};
