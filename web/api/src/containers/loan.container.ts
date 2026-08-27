19af464e3d92afa951faa792b83f47f1ef7cde8d
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-08-27 17:59:29 +0500

    Update loan.container.ts

--- web/api/src/containers/loan.container.ts
+++ web/api/src/containers/loan.container.ts
@@ -1,11 +1,19 @@
 import { LoanController } from '../controllers/loan.controller.js';
+import { ContactRepository } from '../repository/contact.repository.js';
+import { ItemRepository } from '../repository/item.repository.js';
 import { LoanRepository } from '../repository/loan.repository.js';
 import { LoanRoute } from '../routes/loan.route.js';
 import { LoanService } from '../services/loan.service/loan.service.js';
 
 export const createLoanModule = () => {
+  const itemRepository = new ItemRepository();
+  const contactRepository = new ContactRepository();
   const loanRepository = new LoanRepository();
-  const loanService = new LoanService(loanRepository);
+  const loanService = new LoanService(
+    loanRepository,
+    itemRepository,
+    contactRepository,
+  );
   const loanController = new LoanController(loanService);
 
   return new LoanRoute(loanController);
