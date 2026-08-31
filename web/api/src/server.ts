06c2a5cecf94d67406432b191ba06086bdb34522
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-08-31 19:39:16 +0500

    Update server.ts

--- web/api/src/server.ts
+++ web/api/src/server.ts
@@ -1,6 +1,7 @@
 import app from './app.js';
 import connectDB from './db/connect.js';
 import { config } from './config/config.js';
+import { startReminderScheduler } from './services/Reminder.scheduler/reminder.scheduler.js';
 
 const port = config.server.port;
 
@@ -15,6 +16,7 @@ const start = async () => {
     await connectDB(mongoURI);
     app.listen(port, () => {
       console.log(`Server is listening at port ${port}...`);
+      startReminderScheduler();
     });
   } catch (error) {
     if (error instanceof Error) {
