e1ba92963e1b71e2ea3350dfb8508999fcfca173
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-09-15 15:02:47 +0500

    Create registry.ts

---
+++ web/api/src/openapi/registry.ts
@@ -0,0 +1,14 @@
+// openapi/registry.ts
+import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
+
+export const registry = new OpenAPIRegistry();
+
+export const bearerAuth = registry.registerComponent(
+  'securitySchemes',
+  'bearerAuth',
+  {
+    type: 'http',
+    scheme: 'bearer',
+    bearerFormat: 'JWT',
+  },
+);
