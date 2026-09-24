034a1f198075fbb717ee96ff1cc79e9b8b017ef3
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-09-24 12:41:00 +0500

    Update AppProvider.tsx

--- web/app/src/useContext/AppProvider.tsx
+++ web/app/src/useContext/AppProvider.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react';
-import { AppContext } from './appContext';
+import { AppContext } from './AppContext';
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   const [darkMode, setDarkMode] = useState(true);
 
   useEffect(() => {
     if (darkMode) {
-      document.body.classList.add('bg-DBackground');
-      document.body.classList.remove('bg-LBackground');
+      document.body.classList.add('bg-d-background');
+      document.body.classList.remove('bg-l-background');
     } else {
-      document.body.classList.remove('bg-DBackground');
-      document.body.classList.add('bg-LBackground');
+      document.body.classList.remove('bg-d-background');
+      document.body.classList.add('bg-l-background');
     }
   }, [darkMode]);
 
